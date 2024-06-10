3cc0121272debc1ab5d97d848aeaf8dde2fca202
Aliaksei <sladkiy.aleksey@gmail.com>
2024-06-10 16:30:16 +0200

    feat: add cdk deploy

---
+++ aws/lib/aws-sqs.ts
@@ -0,0 +1,73 @@
+import { Construct } from "constructs";
+import { CfnOutput, RemovalPolicy } from "aws-cdk-lib";
+import { Distribution, PriceClass, ViewerProtocolPolicy } from "aws-cdk-lib/aws-cloudfront";
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+
+export class Sqs extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.init();
+  }
+
+  private init() {
+    const bucket = this.createBucket();
+    const distribution = this.createDistribution(bucket);
+
+    this.deployBucket(bucket, distribution);
+    this.createCloudFrontDistribution(bucket, distribution);
+  }
+
+  private createBucket(): Bucket {
+    return new Bucket(this, "shopReactBucket", {
+      versioned: true,
+      autoDeleteObjects: true,
+      bucketName: "aws-shop-react-rs-cdk",
+      removalPolicy: RemovalPolicy.DESTROY,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    });
+  }
+
+  private createDistribution(bucket: Bucket): Distribution {
+    return new Distribution(this, "ShopReactDistribution", {
+      defaultRootObject: "index.html",
+      defaultBehavior: {
+        origin: new S3Origin(bucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      priceClass: PriceClass.PRICE_CLASS_100,
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: "/index.html",
+        },
+      ],
+    });
+  }
+
+  private deployBucket(bucket: Bucket, distribution: Distribution): void {
+    new BucketDeployment(this, "BucketDeployment", {
+      distribution,
+      destinationBucket: bucket,
+      distributionPaths: ["/*"],
+      sources: [Source.asset('../dist')],
+    });
+  }
+
+  private createCloudFrontDistribution(bucket: Bucket, distribution: Distribution): void {
+      new CfnOutput(this, "CloudFrontURL", {
+      value: distribution.domainName,
+      description: "The distribution URL",
+      exportName: "CloudfrontURL",
+    });
+
+    new CfnOutput(this, "BucketName", {
+      value: bucket.bucketName,
+      description: "The name of the S3 bucket",
+      exportName: "BucketName",
+    });
+  }
+}
